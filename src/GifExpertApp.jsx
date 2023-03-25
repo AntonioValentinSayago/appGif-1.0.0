bffe0861cdd3fef492603fa2709acdf34fc17521
AntonioValentin <cesar_anto30@hotmail.com>
2023-03-25 02:12:15 -0600

    Update Component AddCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,7 +4,7 @@ import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch']);
+    const [categories, setCategories] = useState(['']);
 
     const onAddCategory = (newCategory ) =>{
 
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,8 +1,10 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=vLJiOZ6gLDp1L1dLoHJYZ60JWEP60Lh1&q=${category}&limit=10`;
+/* Fetching the data from the API. */
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=vLJiOZ6gLDp1L1dLoHJYZ60JWEP60Lh1&q=${category}&limit=1`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
+/* Mapping the data from the API to the gifs array. */
     const gifs = data.map(img => ({
         id: img.id,
         title: img.title,
